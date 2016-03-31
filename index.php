8ae97e1ccb54c84397bd7f26d2a6e91c2a077462
Perry <teckdeck114@hotmail.no>
2016-03-31 11:50:54 +0200

    Patch 0.0.3
    Patcha, fikset mobil side. (link inn til mobil side og data siden.) (Index og Home er blitt forandra i mobilversjonen, og de andre .php sidene!)

--- index.php
+++ index.php
@@ -9,35 +9,49 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-	<title>Elevpc system v.0.1</title>
+<meta charset=utf8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<center>
+<title>Elevpc system v.0.1</title>
 <img src="/images/pclogo.png" alt="Laptop" height="120" width="120">
 		<h1>Elevpc System v.0.1</h1>
 			<link rel="stylesheet" media="screen" href="login.css" >
-</head>
-<body>
+ 
+
 <form class="contact_form" action="verify.php" method="post" name="contact_form">
-    <ul>
-        <li>
              <h2>Vennligst logg inn</h2>
-             <span class="required_notification">* Viktige felter</span>
-        </li>
+             <span class="required_notification">* Viktige felter</span> </li>
+
         <li>
             <label for="name">Brukernavn:</label>
             <input type="text"  placeholder="Joe" required  name="name"/>
-	    <span class="form_hint">Proper format "Joe"</span>
+	    <h4 class="form_hint">Proper format "Joe"</h4>
         </li>
-	 <li>
+		
+    <li>
             <label for="name">Passord:</label>
             <input type="password"  placeholder="Dinagat" required name="pass"/>
 	    <span class="form_hint">Proper format "xxxxxxx"</span>
-        </li>
-        <li>
-        	<button class="submit" type="submit">Login</button>
-        </li>
-    </ul>
+    </li>
 
-<span><center> Lagd av Per Arne Dahl Kristiansen, 2016 (c), 1SSA, RVS </center></span>
-</form>
-</body>
+       <button class="submit" type="submit">Login</button>
+</center>
 </html>
+
+
+<h3><center> Lagd av Per Arne Dahl Kristiansen, 2016 (c), 1SSA, RVS</center><h3>
+<center>
+<a href="http://www.000webhost.com/" target="_blank"><img src="http://www.000webhost.com/images/120x60_powered.gif" alt="Web Hosting" width="108" height="54" border="0" /></a>
+<a href="http://github.com/ItsaPerryBob/RVS-DB/" target="_blank"><img src="http://www.rvs-db.netau.net/images/github.png" alt="Web Hosting" width="108" height="54" border="0" />
+
+<h3> Ser etter dataversjonen?<h3>
+
+		<a href="http:/rvs-db.netau.net/" title="Her">Trykk Her</a>
+</center>
+
+
+
+
+
+
---
+++ pdf.php
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve">
+  <fields>
+    <field name="first_name">
+      <value>John</value>
+    </field>
+    <field name="last_name">
+      <value>Doe</value>
+    </field>
+    <field name="email">
+      <value>jdoe@example.com</value>
+    </field>
+    <field name="dob">
+      <value>1980-04-14</value>
+    </field>
+    <field name="notes">
+      <value>It's a plain text field that contains stuff.</value>
+    </field>
+  </fields>
+  <ids original="d8b5fb00b50e5bf5caca4aa92ea9253b" modified="1363280525" />
+  <f href="http://rvs-db.netau.net/Lenovo.pdf" />
+</xfdf>
